d781b03adfe34aadec4f9f79b6a1c49cefd865b4
shoban <shobanbabu.dharavath@phenompeople.com>
2023-01-18 17:20:09 +0530

    "changed code"

--- demo/src/main/java/com/example/demo/Model/StudentsModel.java
+++ demo/src/main/java/com/example/demo/Model/StudentsModel.java
@@ -2,8 +2,6 @@ package com.example.demo.Model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import jakarta.annotation.Generated;
 import lombok.Data;
 
 @Data
--- demo/src/main/java/com/example/demo/Repository/Studentsrepository.java
+++ demo/src/main/java/com/example/demo/Repository/Studentsrepository.java
@@ -1,10 +1,7 @@
 package com.example.demo.Repository;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.Model.StudentsModel;
